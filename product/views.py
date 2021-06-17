ef657252681571189b17f93b49c5b7f4d76e08cc
marx7x3 <m.ysakov.jcc@gmail.com>
2021-06-17 12:39:50 +0600

    Status pfoduct

--- product/views.py
+++ product/views.py
@@ -13,6 +13,34 @@ class ProductListView(ListView):
     template_name = 'product/products.html'
     context_object_name = 'products'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """Get the context for this view."""
+        queryset = object_list if object_list is not None else self.object_list
+        page_size = self.get_paginate_by(queryset)
+        context_object_name = self.get_context_object_name(queryset)
+        categories = Category.objects.all()
+        if page_size:
+            paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, page_size)
+            context = {
+                'paginator': paginator,
+                'page_obj': page,
+                'is_paginated': is_paginated,
+                'object_list': queryset,
+                'categories': categories
+            }
+        else:
+            context = {
+                'paginator': None,
+                'page_obj': None,
+                'is_paginated': False,
+                'object_list': queryset,
+                'categories': categories
+            }
+        if context_object_name is not None:
+            context[context_object_name] = queryset
+        context.update(kwargs)
+        return super().get_context_data(**context)
+
 
 class ProductDetailView(DetailView):
     model = Product
